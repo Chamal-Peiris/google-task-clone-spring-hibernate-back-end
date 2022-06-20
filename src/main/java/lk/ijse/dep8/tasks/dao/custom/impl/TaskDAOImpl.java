a804377f0b6847e8eaf1b0bb95fbe0822c532ad8
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-20 12:18:49 +0530

    Revamp DAo impl class with @Repository annotation

--- src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskDAOImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskDAOImpl.java
@@ -7,11 +7,12 @@ import org.hibernate.Session;
 import org.springframework.context.annotation.Scope;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
 @Scope("prototype")
-@Component
+@Repository
 public class TaskDAOImpl extends CrudDAOImpl<Task, Integer> implements TaskDAO {
 
     public TaskDAOImpl(@Nullable EntityManager em) {
--- src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskListDAOImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/custom/impl/TaskListDAOImpl.java
@@ -7,11 +7,12 @@ import org.hibernate.Session;
 import org.springframework.context.annotation.Scope;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
 @Scope("prototype")
-@Component
+@Repository
 public class TaskListDAOImpl extends CrudDAOImpl<TaskList, Integer> implements TaskListDAO {
 
     public TaskListDAOImpl(@Nullable EntityManager em) {
