179efcd30fb3d03ec17f792dbb435beadd7af14b
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-20 12:48:08 +0530

    Revamp service  layer

--- src/main/java/lk/ijse/dep8/tasks/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/ijse/dep8/tasks/service/custom/impl/UserServiceImpl.java
@@ -6,15 +6,12 @@ import lk.ijse.dep8.tasks.entity.User;
 import lk.ijse.dep8.tasks.service.custom.UserService;
 import lk.ijse.dep8.tasks.service.exception.FailedExecutionException;
 import lk.ijse.dep8.tasks.service.util.EntityDTOMapper;
-import lk.ijse.dep8.tasks.service.util.HibernateUtil;
-import lk.ijse.dep8.tasks.util.JPAUtil;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -26,27 +23,24 @@ import java.util.logging.Logger;
 
 @Scope("prototype")
 @Component
+@Transactional
 public class UserServiceImpl implements UserService {
     private final Logger logger = Logger.getLogger(UserServiceImpl.class.getName());
     @Autowired
     private UserDAO userDAO;
 
     public boolean existsUser(String userIdOrEmail) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        try {
-            userDAO.setEntityManager(em);
+
             return userDAO.existsUserByEmailOrId(userIdOrEmail);
-        } finally {
-            em.close();
-        }
+
     }
 
     public UserDTO registerUser(Part picture,
                                 String appLocation,
                                 UserDTO user) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+
         try {
-            em.getTransaction().begin();
+
             user.setId(UUID.randomUUID().toString());
 
             if (picture != null) {
@@ -54,7 +48,7 @@ public class UserServiceImpl implements UserService {
             }
             user.setPassword(DigestUtils.sha256Hex(user.getPassword()));
 
-            userDAO.setEntityManager(em);
+
             // DTO -> Entity
             User userEntity = EntityDTOMapper.getUser(user);
             User savedUser = userDAO.save(userEntity);
@@ -71,36 +65,27 @@ public class UserServiceImpl implements UserService {
                 picture.write(picturePath);
             }
 
-            em.getTransaction().commit();
+
             return user;
         } catch (Throwable t) {
-            if (em != null && em.getTransaction() != null) {
-                em.getTransaction().rollback();
-            }
+
             throw new FailedExecutionException("Failed to save the user", t);
-        } finally {
-            em.close();
         }
     }
 
     public UserDTO getUser(String userIdOrEmail) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        try {
-            userDAO.setEntityManager(em);
+
             Optional<User> userWrapper = userDAO.findUserByIdOrEmail(userIdOrEmail);
             return EntityDTOMapper.getUserDTO(userWrapper.orElse(null));
-        } finally {
-            em.close();
-        }
+
     }
 
     public void deleteUser(String userId, String appLocation) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+
         try {
-            em.getTransaction().begin();
-            userDAO.setEntityManager(em);
+
             userDAO.deleteById(userId);
-            em.getTransaction().commit();
+
 
             new Thread(() -> {
                 Path imagePath = Paths.get(appLocation, "uploads",
@@ -112,23 +97,20 @@ public class UserServiceImpl implements UserService {
                 }
             }).start();
         } catch (Throwable t) {
-            if (em != null && em.getTransaction() != null) em.getTransaction().rollback();
-            throw new FailedExecutionException("Failed to delete the user", t);
-        } finally {
-            em.close();
+          throw new FailedExecutionException("Failed to delete the user",t);
         }
 
     }
 
     public void updateUser(UserDTO user, Part picture,
                            String appLocation) {
-        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+
         try {
-            em.getTransaction().begin();
+
 
             user.setPassword(DigestUtils.sha256Hex(user.getPassword()));
 
-            userDAO.setEntityManager(em);
+
 
             // Fetch the current user
             User userEntity = userDAO.findById(user.getId()).get();
@@ -153,12 +135,10 @@ public class UserServiceImpl implements UserService {
                 Files.deleteIfExists(picturePath);
             }
 
-            em.getTransaction().commit();
+
         } catch (Throwable e) {
-            if (em != null && em.getTransaction() != null) em.getTransaction().rollback();
+
             throw new FailedExecutionException("Failed to update the user", e);
-        } finally {
-            em.close();
         }
     }
 
